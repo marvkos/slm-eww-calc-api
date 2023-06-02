bad497dcfbaf491ac890fba637ab77eb262d1c7f
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2023-06-02 13:41:29 +0200

    inital commit

---
+++ src/main/java/at/technikum/slmewwcalcapi/SlmEwwCalcApiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.slmewwcalcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmEwwCalcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmEwwCalcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/technikum/slmewwcalcapi/SlmEwwCalcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slmewwcalcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmEwwCalcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
